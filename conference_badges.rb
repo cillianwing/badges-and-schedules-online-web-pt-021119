1d3355b62dad614388e3d824558a1f23c466c41f
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-02-18 17:20:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,8 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(speakers)
-  speakers.collect_with_index do |name, index|
-    return "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  final_array = []
+  speakers.each_with_index do |name, index|
+    final_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
